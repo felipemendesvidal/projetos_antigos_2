0fd547c17f7d690fb2f1c214306437ba4702a32e
felipe vidal <contatofelipevidalmendes@gmail.com>
2022-01-18 08:54:14 -0200

    colegio cordeiro

---
+++ colegio cordeiro 2020/assets/js/script-galeria.js
@@ -0,0 +1,650 @@
+// Arrow Function Ready to JQuery
+$(() => {
+  
+  // Array de Objetos con información de las imágenes
+  let dataImages = [
+  
+    
+    
+    // {
+    //   "id": 7,
+    //   "description": "Estrutura - Fotos",
+    //   "tabletImg": {
+    //     "height": "300",
+    //     "width": "500",
+    //     "url": "assets/img/estrutura/estrutura(5).jpg"
+    //   },
+    //   "thumbImg": {
+    //     "height": "300",
+    //     "width": "500",
+    //     "url": "assets/img/estrutura/estrutura(5).jpg"
+    //   }
+    // },
+    // {
+    //   "id": 12,
+    //   "description": "Estrutura - Fotos",
+    //   "tabletImg": {
+    //     "height": "300",
+    //     "width": "500",
+    //     "url": "assets/img/estrutura/estrutura(6).jpg"
+    //   },
+    //   "thumbImg": {
+    //     "height": "300",
+    //     "width": "500",
+    //     "url": "assets/img/estrutura/estrutura(6).jpg"
+    //   }
+    // },
+    {
+      "id": 12,
+      "description": "Estrutura - Fotos",
+      "tabletImg": {
+        "height": "500",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(7).jpg"
+      },
+      "thumbImg": {
+        "height": "500",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(7).jpg"
+      }
+    },
+    {
+      "id": 12,
+      "description": "Estrutura - Fotos",
+      "tabletImg": {
+        "height": "500",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(31).jpg"
+      },
+      "thumbImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(31).jpg"
+      }
+    },
+    {
+      "id": 12,
+      "description": "Estrutura - Fotos",
+      "tabletImg": {
+        "height": "500",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(30).jpg"
+      },
+      "thumbImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(30).jpg"
+      }
+    },
+      
+     {
+      "id": 12,
+      "description": "Estrutura - Fotos",
+      "tabletImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(9).jpg"
+      },
+      "thumbImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(9).jpg"
+      }
+    },
+     {
+      "id": 12,
+      "description": "Estrutura - Fotos",
+      "tabletImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(10).jpg"
+      },
+      "thumbImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(10).jpg"
+      }
+    },
+     {
+      "id": 12,
+      "description": "Estrutura - Fotos",
+      "tabletImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(12).jpg"
+      },
+      "thumbImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(12).jpg"
+      }
+    },
+      {
+      "id": 12,
+      "description": "Estrutura - Fotos",
+      "tabletImg": {
+        "height": "500",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(14).jpg"
+      },
+      "thumbImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(14).jpg"
+      }
+    },
+    {
+      "id": 12,
+      "description": "Estrutura - Fotos",
+      "tabletImg": {
+        "height": "500",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(32).jpg"
+      },
+      "thumbImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(32).jpg"
+      }
+    },
+    {
+      "id": 12,
+      "description": "Estrutura - Fotos",
+      "tabletImg": {
+        "height": "500",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(33).jpg"
+      },
+      "thumbImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(33).jpg"
+      }
+    },
+    {
+      "id": 12,
+      "description": "Estrutura - Fotos",
+      "tabletImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(16).jpg"
+      },
+      "thumbImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(16).jpg"
+      }
+    },
+        {
+      "id": 12,
+      "description": "Estrutura - Fotos",
+      "tabletImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(13).jpg"
+      },
+      "thumbImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(13).jpg"
+      }
+    },
+        {
+      "id": 12,
+      "description": "Estrutura - Fotos",
+      "tabletImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(19).jpg"
+      },
+      "thumbImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(19).jpg"
+      }
+    },
+    {
+      "id": 12,
+      "description": "Estrutura - Fotos",
+      "tabletImg": {
+        "height": "500",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(34).jpg"
+      },
+      "thumbImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(34).jpg"
+      }
+    },
+    {
+      "id": 12,
+      "description": "Estrutura - Fotos",
+      "tabletImg": {
+        "height": "500",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(35).jpg"
+      },
+      "thumbImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(35).jpg"
+      }
+    },
+      {
+      "id": 12,
+      "description": "Estrutura - Fotos",
+      "tabletImg": {
+        "height": "500",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(30).jpg"
+      },
+      "thumbImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(30).jpg"
+      }
+    },
+    {
+      "id": 12,
+      "description": "Estrutura - Fotos",
+      "tabletImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(20).jpg"
+      },
+      "thumbImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(20).jpg"
+      }
+    },
+        {
+      "id": 12,
+      "description": "Estrutura - Fotos",
+      "tabletImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(21).jpg"
+      },
+      "thumbImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(21).jpg"
+      }
+    },
+      {
+      "id": 12,
+      "description": "Estrutura - Fotos",
+      "tabletImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(23).jpg"
+      },
+      "thumbImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(23).jpg"
+      }
+    },
+    {
+      "id": 1,
+      "description": "Estrutura - Fotos",
+      "tabletImg": {
+        "height": "500",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(35).jpg"
+      },
+      "thumbImg": {
+        "height": "auto",
+        "width": "auto",
+        "url": "assets/img/estrutura/estrutura(35).jpg"
+      }
+    },
+    {
+      "id": 2,
+      "description": "Estrutura - Fotos",
+      "tabletImg": {
+        "height": "500",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(36).jpg"
+      },
+      "thumbImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(36).jpg"
+      }
+    },
+    {
+      "id": 4,
+      "description": "Estrutura - Fotos",
+      "tabletImg": {
+        "height": "500",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(37).jpg"
+      },
+      "thumbImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(37).jpg"
+      }
+    },
+      {
+      "id": 12,
+      "description": "Estrutura - Fotos",
+      "tabletImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(26).jpg"
+      },
+      "thumbImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(26).jpg"
+      }
+    },
+        
+          {
+      "id": 12,
+      "description": "Estrutura - Fotos",
+      "tabletImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(28).jpg"
+      },
+      "thumbImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(28).jpg"
+      }
+    },
+          {
+      "id": 12,
+      "description": "Estrutura - Fotos",
+      "tabletImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(29).jpg"
+      },
+      "thumbImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(29).jpg"
+      }
+    },
+    {
+      "id": 1,
+      "description": "Estrutura - Fotos",
+      "tabletImg": {
+        "height": "500",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(38).jpg"
+      },
+      "thumbImg": {
+        "height": "auto",
+        "width": "auto",
+        "url": "assets/img/estrutura/estrutura(38).jpg"
+      }
+    },
+    {
+      "id": 2,
+      "description": "Estrutura - Fotos",
+      "tabletImg": {
+        "height": "500",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(39).jpg"
+      },
+      "thumbImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(39).jpg"
+      }
+    },
+    {
+      "id": 4,
+      "description": "Estrutura - Fotos",
+      "tabletImg": {
+        "height": "500",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(40).jpg"
+      },
+      "thumbImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(40).jpg"
+      }
+    },
+    {
+      "id": 1,
+      "description": "Estrutura - Fotos",
+      "tabletImg": {
+        "height": "500",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(41).jpg"
+      },
+      "thumbImg": {
+        "height": "auto",
+        "width": "auto",
+        "url": "assets/img/estrutura/estrutura(41).jpg"
+      }
+    },
+    {
+      "id": 2,
+      "description": "Estrutura - Fotos",
+      "tabletImg": {
+        "height": "500",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(45).jpg"
+      },
+      "thumbImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(45).jpg"
+      }
+    },
+    {
+      "id": 4,
+      "description": "Estrutura - Fotos",
+      "tabletImg": {
+        "height": "500",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(44).jpg"
+      },
+      "thumbImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(44).jpg"
+      }
+    },
+    {
+      "id": 4,
+      "description": "Estrutura - Fotos",
+      "tabletImg": {
+        "height": "500",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(46).jpg"
+      },
+      "thumbImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(46).jpg"
+      }
+    },
+    {
+      "id": 4,
+      "description": "Estrutura - Fotos",
+      "tabletImg": {
+        "height": "500",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(47).jpg"
+      },
+      "thumbImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(47).jpg"
+      }
+    },
+    {
+      "id": 4,
+      "description": "Estrutura - Fotos",
+      "tabletImg": {
+        "height": "500",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(48).jpg"
+      },
+      "thumbImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(48).jpg"
+      }
+    },
+    {
+      "id": 4,
+      "description": "Estrutura - Fotos",
+      "tabletImg": {
+        "height": "500",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(49).jpg"
+      },
+      "thumbImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(49).jpg"
+      }
+    },
+    {
+      "id": 4,
+      "description": "Estrutura - Fotos",
+      "tabletImg": {
+        "height": "500",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(50).jpg"
+      },
+      "thumbImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(50).jpg"
+      }
+    },
+    {
+      "id": 4,
+      "description": "Estrutura - Fotos",
+      "tabletImg": {
+        "height": "500",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(52).jpg"
+      },
+      "thumbImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(52).jpg"
+      }
+    },
+     {
+      "id": 4,
+      "description": "Estrutura - Fotos",
+      "tabletImg": {
+        "height": "500",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(53).jpg"
+      },
+      "thumbImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(53).jpg"
+      }
+    },
+    {
+      "id": 4,
+      "description": "Estrutura - Fotos",
+      "tabletImg": {
+        "height": "500",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(54).jpg"
+      },
+      "thumbImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(54).jpg"
+      }
+    },
+    {
+      "id": 4,
+      "description": "Estrutura - Fotos",
+      "tabletImg": {
+        "height": "500",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(56).jpg"
+      },
+      "thumbImg": {
+        "height": "300",
+        "width": "500",
+        "url": "assets/img/estrutura/estrutura(56).jpg"
+      }
+    },
+     
+
+
+
+    
+  ]
+  
+  // Función que ejecutará ImagesLoaded, Masonry y Photoswipe
+  let photoSwipe = () => {
+    var $container = $('.grid').imagesLoaded(function(){
+      $container.masonry({
+        itemSelector: '.item',
+        columnWidth: '.grid-sizer',
+        percentPosition: true
+      });
+    });
+    var container = [];
+    $('#gallery').find('figure.item').each(function(){
+      var $link = $(this).find('a.img-pswp'),
+          item = {
+            src: $link.attr('href'),
+            w: $link.data('width'),
+            h: $link.data('height'),
+            title: $link.data('caption')
+          };
+      container.push(item);
+    });
+    $('a.img-pswp').click(function(event){
+      event.preventDefault();
+      var $pswp = $('.pswp')[0],
+          options = {
+            index: $(this).parent('figure.item').index(),
+            bgOpacity: 0.85,
+            showHideOpacity: true,
+            showAnimationDuration: true
+          };
+      var gallery = new PhotoSwipe($pswp, PhotoSwipeUI_Default, container, options);
+      gallery.init();
+    });
+  },
+  // Template HTML para las imágenes
+  template = (el, elFilter) => {
+    let tmpl = `
+      <figure class="item" itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
+        <a class="img-pswp" href="${elFilter.url}" data-width="${elFilter.width}" data-height="${elFilter.height}" data-caption="${el.description}" itemprop="contentUrl" pid="${el.id}">
+          <img id="${el.id}" src="${el.thumbImg.url}" itemprop="thumbnail" alt="${el.description}">
+        </a>
+      </figure>
+    `;
+    return tmpl;
+  },
+  keys = Object.keys(dataImages).map(key => dataImages[key]).reverse()
+  
+  // Agrego las imágenes al DOM y ejecuto la función Photoswipe
+  if($(window).innerWidth() < 577){
+    keys.forEach(data => $('#gallery').prepend(template(data, data.thumbImg)))
+    photoSwipe()
+  } else{
+    keys.forEach(data => $('#gallery').prepend(template(data, data.tabletImg)))
+    photoSwipe()
+  }
+  
+  // Línea que ejecuta el Parallax
+  $('figure.item').each(i => setTimeout(() => $('figure.item').eq(i).addClass('show-items'), 150 * (i+1)))
+  
+})
